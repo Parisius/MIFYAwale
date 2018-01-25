2789e5335fbadf3d37c99cef0afa82433350094a
Parisius <parisiushss03@gmail.com>
2018-01-25 05:58:53 +0100

    Initial commit

---
+++ app/src/main/java/com/mifyai/mifyawale/ArtificialIntelligence.java
@@ -0,0 +1,17 @@
+package com.mifyai.mifyawale;
+
+/**
+ * This interface define function used by AI algoritms.
+ * Created by Parikiri13 on 25/01/2018.
+ */
+
+public interface ArtificialIntelligence {
+
+    /**
+     * Searches the best position to play for the current state of the awale on
+     * the given side.
+     *
+     * @return the best position or <code>Short.MIN_VALUE</code>, if none is found
+     */
+    public short searchBestPlay(Awale awale, short side);
+}
---
+++ app/src/main/java/com/mifyai/mifyawale/MainActivity.java
@@ -0,0 +1,89 @@
+package com.mifyai.mifyawale;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import utils.ReportSender;
+import view.ViewTools;
+
+/**
+ * This activity display the main menu.
+ *
+ * @author Marie-Parisius HOUESSOU
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //ReportSender.install(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
+       ViewTools.inflateView(this, R.layout.awale_layout);
+
+        Typeface outOfAfricaTypeFace = Typeface.createFromAsset(getAssets(),
+                "fonts/out_of_africa.ttf");
+
+        TextView awaleTitleTextView = (TextView) findViewById(R.id.awale_title);
+        awaleTitleTextView.setTypeface(outOfAfricaTypeFace);
+
+        Typeface africanTypeFace = Typeface.createFromAsset(getAssets(),
+                "fonts/african.ttf");
+
+        Button newGameButton = (Button) findViewById(R.id.new_game_button);
+        newGameButton.setTypeface(africanTypeFace);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PlayActivity.class));
+            }
+        });
+
+        Button rulesButton = (Button) findViewById(R.id.rules_button);
+        rulesButton.setTypeface(africanTypeFace);
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RulesActivity.class));
+            }
+        });
+
+        Button creditsButton = (Button) findViewById(R.id.credits_button);
+        creditsButton.setTypeface(africanTypeFace);
+        creditsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,
+                        CreditsActivity.class));
+            }
+        });
+
+        PackageManager pm = getPackageManager();
+        PackageInfo pi;
+        try {
+            pi = pm.getPackageInfo(getPackageName(), 0);
+            TextView demoVersionButton = (TextView) findViewById(R.id.demo_version);
+            demoVersionButton.setText(getString(R.string.demo_version) + " "
+                    + pi.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        super.onCreate(savedInstanceState);
+    }
+    // @Override
+    // protected void onNewIntent(Intent intent) {
+    // boolean mustResume = intent.getExtras().getBoolean(RESUME_KEY, false);
+    // if (mustResume) {
+    // startActivity(new Intent(MainActivity.this, PlayActivity.class));
+    // }
+    // cancelPendingItent();
+    // super.onNewIntent(intent);
+    // }
+}
